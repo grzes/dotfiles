3e81c5cd9a3b3a99a3dc8509886692708f128b67
Grzes Furga <grzesiof@gmail.com>
2011-05-29 15:15:57 +0200

    First commit to dotfiles repo.

---
+++ install.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import os
+import re
+import shutil
+import hashlib
+from optparse import OptionParser
+
+re_dot = re.compile(r'^\.[^\.]+$')
+
+HOME = os.environ["HOME"]
+
+
+def file_hash(f):
+    return hashlib.md5(open(f).read()).hexdigest()
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-l", "--link-only", dest="link_only",
+        action="store_true", default=False,
+        help="just link the .dotfiles")
+    parser.add_option("-f", "--force", dest="force",
+        action="store_true", default=False,
+        help="don't back up")
+    parser.add_option("-c", "--clean", dest="clean",
+        action="store_true", default=False,
+        help="cleanup backups")
+
+    options, args = parser.parse_args()
+
+    d = os.path.dirname(os.path.abspath(__file__))
+    for f in os.listdir(d):
+        if re_dot.match(f) and f != ".git":
+            new_f = os.path.join(d, f)
+            old_f = os.path.join(HOME, f)
+            print "[%s]" % old_f
+
+            if os.path.exists(old_f):
+                if options.force:
+                    os.remove(old_f)
+                else:
+                    if file_hash(new_f) == file_hash(old_f):
+                        print "    ...ignoring"
+                        continue
+                    else:
+                        print "    ...backing up"
+                        shutil.move(old_f, old_f+'.old')
+
+            if options.clean and os.path.exists(old_f+'.old'):
+                os.remove(old_f+'.old')
+
+            print "    ...symlinking"
+            os.symlink(new_f, old_f)
+
+
+    if not options.link_only:
+        print "Switching to zsh!"
+        os.system("chsh -s /bin/zsh")
+
+
+
+
